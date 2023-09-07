f523556cb5c9da17234412a64cec0c85f2874df4
Sven Grundmann <sgrundmannwbs@gmail.com>
2023-09-07 15:21:34 +0200

    too much to enter

--- src/component/ArticleGames.jsx
+++ src/component/ArticleGames.jsx
@@ -1,10 +1,27 @@
 import { useState, useEffect } from "react";
 import useContentful from "../hooks/useContentful";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BLOCKS, INLINES } from "@contentful/rich-text-types";
 
 function ArticleGames() {
   const [articles, setArticles] = useState();
   const { getContent } = useContentful();
+
+  const renderOptions = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
+        // render the EMBEDDED_ASSET as you need
+        return (
+          <img
+            src={`https://${node.data.target.fields.file.url}`}
+            height={node.data.target.fields.file.details.image.height}
+            width={node.data.target.fields.file.details.image.width}
+            alt={node.data.target.fields.description}
+          />
+        );
+      },
+    },
+  };
 
   useEffect(() => {
     getContent("article").then((response) => setArticles(response));
@@ -22,7 +39,10 @@ function ArticleGames() {
               return (
                 <div className="article" key={key}>
                   <h2>{article.fields.title}</h2>
-                  {documentToReactComponents(article.fields.richText)}
+                  {documentToReactComponents(
+                    article.fields.richText,
+                    renderOptions
+                  )}
                 </div>
               );
             })
--- src/component/Games.jsx
+++ src/component/Games.jsx
@@ -3,17 +3,32 @@ import useContentful from "../hooks/useContentful";
 import React from "react";
 
 function Games() {
-  const [games, setGames] = useState([]);
+  const [games, setGames] = useState();
   const { getContent } = useContentful();
 
   useEffect(() => {
     getContent("game").then((response) => setGames(response));
   }, []);
 
+  if (!games) {
+    return "Loading...";
+  }
+
   return (
     <>
       <h1>Games</h1>
       <div>List of games here!</div>
+      {games
+        ? games.items.map((game, key) => {
+            return (
+              <div className="games" key={key}>
+                <h2>{game.fields.name}</h2>
+                <div>{game.fields.published}</div>
+                <div>{game.fields.rating}</div>
+              </div>
+            );
+          })
+        : "No Results"}
     </>
   );
 }
