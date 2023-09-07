a1f3bf05c45eb3701e29caf7115a87106b572da7
Sven Grundmann <sgrundmannwbs@gmail.com>
2023-09-07 13:37:38 +0200

    FirstTsirFCommitttted

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState, useEffect } from "react";
-import useContentful from "./hooks/useContentful";
 import "./App.css";
 import { Routes, Route, NavLink } from "react-router-dom";
 import NavBar from "./component/NavBar";
